bf88c4f79f78e077b461bd985131e6992bbbda35
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-10-06 09:28:25 -0700

    added highlighted page buttons

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -8,16 +8,21 @@ import PostFood from "./PostFood"
 
 const Sidenav = () => {
     const router = useRouter()
+    
+    const isHome = router.pathname === "/"
+    const isNeighbourhood = router.pathname === "/neighbourhood"
+    const isLeaderboard = router.pathname === "/leaderboard"
+
     return (
         <Flex h={'90vh'} flexDir={'column'} alignItems={'center'} w={'100%'} borderRight={'2px solid #F1F1F1'}>
             <Flex w={"100%"} h={'10vh'} mt={'30px'}>
-                <Button onClick={() => router.push('/')} justifyContent={'left'} w={'80%'} bgColor={'white'} ml={'10%'} leftIcon={<Icon as={IoHomeSharp}/>}>Home</Button>
+                <Button onClick={() => router.push('/')} borderRadius={'100px'} justifyContent={'left'} w={'80%'} bgColor={isHome ? '#f1f1f1' : 'white'} color={isHome ? '#606060' : 'black'} ml={'10%'} leftIcon={<Icon as={IoHomeSharp}/>}>Home</Button>
             </Flex>
             <Flex w={"100%"} h={'10vh'} mt={2}>
-                <Button onClick={() => router.push('/neighbourhood')} justifyContent={'left'} w={'80%'} bgColor={'white'} ml={'10%'} leftIcon={<Icon as={GiVillage}/>}>Neighbourhood</Button>
+                <Button onClick={() => router.push('/neighbourhood')} borderRadius={'100px'} justifyContent={'left'} w={'80%'} bgColor={isNeighbourhood ? '#f1f1f1' : 'white'} color={isNeighbourhood ? '#606060' : 'black'} ml={'10%'} leftIcon={<Icon as={GiVillage}/>}>Neighbourhood</Button>
             </Flex>
             <Flex w={"100%"} h={'10vh'} mt={2}>
-                <Button onClick={() => router.push('/leaderboard')} justifyContent={'left'} w={'80%'} bgColor={'white'} ml={'10%'} leftIcon={<Icon as={IoMdTrophy}/>}>Leaderboards</Button>
+                <Button onClick={() => router.push('/leaderboard')} borderRadius={'100px'} justifyContent={'left'} w={'80%'} bgColor={isLeaderboard ? '#f1f1f1' : 'white'} color={isLeaderboard ? '#606060' : 'black'} ml={'10%'} leftIcon={<Icon as={IoMdTrophy}/>}>Leaderboards</Button>
             </Flex>
             <Flex position={'fixed'} bottom={'20px'}>
                 <PostFood/>
