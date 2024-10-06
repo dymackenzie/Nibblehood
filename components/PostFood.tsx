b13109a8f5b960a13f5876c2042b0e93e576f7e1
Adam <adambrett40@gmail.com>
2024-10-06 03:29:30 -0700

    added distance calculator and styling to PostFood

--- components/PostFood.tsx
+++ components/PostFood.tsx
@@ -1,15 +1,17 @@
-import { Button, Editable, EditableTextarea, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, useDisclosure } from "@chakra-ui/react"
+import { Button, Editable, EditableTextarea, Flex, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, useDisclosure } from "@chakra-ui/react"
 import React, { useState } from "react";
 // import { doAddItem } from "../api/addItem";
 import firebase from "firebase/compat/app";
 import axios from "axios";
 import SignUp from "@/pages/signup";
 import { getAuth } from "firebase/auth";
+import Item from "./Item";
 
 const PostFood = () => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [file, setFile] = useState('');
+
 
   const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -36,7 +38,7 @@ const PostFood = () => {
           claimed: false,
           uid: user.uid
         });
-
+      onClose();
     } else {//redirect to login
       console.log('user not authenticated, redirecting to log in (sign up)');
       return (<SignUp />);
@@ -57,37 +59,40 @@ const PostFood = () => {
             <Flex>
               {/* <Flex bgColor={'green'} w={'40%'}>hi</Flex>
               <Flex bgColor={'red'} w={'60%'}>hi</Flex> */}
-              <div className="App">
-                <form onSubmit={handleSubmit} id="addItemForm">
-                  <h2>Add Item:</h2>
-                  <Input type="text"
-                    id="nameInput"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    style={styles.input}
-                    required />
-                  <Textarea
-                    id="textInput"
-                    placeholder="Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    style={styles.description}
-                    required />
+              {/* <div className="App"> */}
+              <form onSubmit={handleSubmit} id="addItemForm">
+                <h2>Add Item:</h2>
+                <Input type="text"
+                  id="nameInput"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  style={styles.input}
+                  required />
+                <Textarea
+                  id="textInput"
+                  placeholder="Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  style={styles.description}
+                  required />
 
-                  <Input type="file" onChange={handleChange} />
-                  <img src={file} />
-                  <Button type="submit" colorScheme="blue" >Post!</Button>
-                </form>
-              </div>
+                <Input type="file" onChange={handleChange} />
+                <Image src={file} style={styles.image} objectFit='cover' boxSize='400px' />
+                {/* <Button type="submit" colorScheme="blue" >Post!</Button> */}
+              </form>
+              {/* </div> */}
+            </Flex>
+            <Flex>
+              <Item item={new } />
             </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
+            <Button type="submit" colorScheme='blue' mr={3} onClick={handleSubmit}>
+              Post!
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
+            {/* <Button variant='ghost'>Secondary Action</Button> */}
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -96,7 +101,7 @@ const PostFood = () => {
 }
 
 const styles = {
-  input: {
+  image: {
 
   },
   button: {
