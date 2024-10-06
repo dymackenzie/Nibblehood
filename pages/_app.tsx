cb463b2fafa7b02e63187825b3c76f00468e0ea2
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-10-06 03:14:18 -0700

    added font and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import "../styles/font.css"
+const theme = extendTheme({
+  fontSizes: {
+    h1: "63px",
+    h2: "40px",
+    h3: "30px",
+    h4: "20px",
+    h5: "20px",
+    h6: "15px",
+    button: "13px",
+    body: "13px"
+  },
+  fonts: {
+    heading: `"ppeditorial"`,
+    body: `"ppeditorial"`
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <Navbar/>
     <Component {...pageProps} />
   </ChakraProvider>
