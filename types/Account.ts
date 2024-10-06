2111ae041cef6340ccb19252666beef12051d1a1
Mackenzie <kenziedy@students.cs.ubc.ca>
2024-10-05 18:45:03 -0700

    ~ changed instances into classes

--- types/Account.ts
+++ types/Account.ts
@@ -1,6 +1,13 @@
-export default interface Account {
-    name: string,
-    email: string,
-    password: string,
-    location: string
+export default class Account {
+    UUID: string;
+    name: string;
+    location: string;
+    neighbourhood: string;
+
+    constructor(uuid: string, name: string, location: string, neighbourhood: string) {
+        this.UUID = uuid;
+        this.name = name;
+        this.location = location;
+        this.neighbourhood = neighbourhood;
+    }
 }
--- types/Item.ts
+++ types/Item.ts
@@ -1,11 +1,22 @@
 import Account from "./Account";
 
-export default interface Item {
-    name: string,
-    description: string,
-    image: string,
-    time: Date,
-    claimed: Boolean,
-    points: number,
-    account: Account
+export default class Item {
+    name: string;
+    description: string;
+    image: string;
+    time: Date;
+    claimed: Boolean;
+    points: number;
+    account: Account;
+
+    constructor(name: string, description: string, image: string, 
+        time: Date, claimed: Boolean, points: number, account: Account) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.time = time;
+        this.claimed = claimed;
+        this.points = points;
+        this.account = account;             
+    }
 }
